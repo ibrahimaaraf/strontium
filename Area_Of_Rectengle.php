9852108338353df91fa0e585bd94f7b88e8e25a9
ibrahimaaraf <60263444+ibrahimaaraf@users.noreply.github.com>
2020-02-08 17:50:39 +0600

    Add files via upload

---
+++ Area_Of_Rectengle.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$length = 10;
+$width = 5;
+$area = $length * $width;
+$perimeter =$length + $width; 
+
+echo "The Area of the Reactangle is " . $area . "<br>";
+echo "The perimeter of the Reactangle is " . $perimeter . "<br>";
+?>
+
+</body>
+</html>
