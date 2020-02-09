756a84996fe53d7e5e906a974e351fd11d6a9ab5
ibrahimaaraf <60263444+ibrahimaaraf@users.noreply.github.com>
2020-02-09 19:12:59 +0600

    L2_qs4

---
+++ Largest_number.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+
+<?php
+
+$num = 11;
+$num1 = 100;
+$num2 = 12;
+
+	if($num>$num1 && $num>$num2)
+		{
+		echo "The largest number is " . $num . "<br>";
+		}
+	else if($num1>$num && $num1>$num2)
+		{
+		echo "The largest number is " . $num1 . "<br>";
+		}
+		else 
+		{
+		echo "The largest number is " . $num2 . "<br>";
+		}
+
+
+?>
+
+</body>
+</html>
